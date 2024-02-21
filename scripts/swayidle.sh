1961da8c2a12e441a6fcff534631b1cd8762bb91
kev <yqlbu@bu.edu>
2024-02-21 09:32:55 +0800

    feat: unmount-samba before going to idle

--- scripts/swayidle.sh
+++ scripts/swayidle.sh
@@ -7,4 +7,4 @@ swayidle -w \
   timeout 300 'swaylock -f' \
   timeout 360 'hyprctl dispatch dpms off' \
   resume 'hyprctl dispatch dpms on' \
-  before-sleep 'swaylock -f' &
+  before-sleep 'unmount-samba && swaylock -f' &
